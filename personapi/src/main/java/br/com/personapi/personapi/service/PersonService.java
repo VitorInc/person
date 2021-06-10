eedcdddb26851ccefa9b5592901c6f4752d553f8
VitorInc <vitordc.contato@gmail.com>
2021-06-09 22:42:44 -0300

    ...

--- personapi/src/main/java/br/com/personapi/personapi/service/PersonService.java
+++ personapi/src/main/java/br/com/personapi/personapi/service/PersonService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
+@Servicepi
 public class PersonService {
 
     private PersonRepository personRepository;
