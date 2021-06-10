4e49c719692bb68dbf17e4453970fd2bee3d0bcf
VitorInc <vitordc.contato@gmail.com>
2021-06-09 23:18:48 -0300

    ...

--- personapi/src/main/java/br/com/personapi/personapi/controller/PersonController.java
+++ personapi/src/main/java/br/com/personapi/personapi/controller/PersonController.java
@@ -6,6 +6,7 @@ import br.com.personapi.personapi.entities.Person;
 import br.com.personapi.personapi.exception.PersonNotFournfExceptiond;
 import br.com.personapi.personapi.repositories.PersonRepository;
 import br.com.personapi.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonRepository personRepository;
@@ -23,10 +25,7 @@ public class PersonController {
   private PersonService personService;
 
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
